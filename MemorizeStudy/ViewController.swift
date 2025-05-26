3ba97d13a36f000d2d1db50bc93773d0a3c3ee32
Vallner <vkwgfh@gmail.com>
2025-05-26 18:23:03 +0300

    -
    initial commit

---
+++ MemorizeStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemorizeStudy
+//
+//  Created by  on 24.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _ =
+        String( int)
+        // Do any additional setup after loading the view.
+    }
+    let int: Int = 3
+
+}
